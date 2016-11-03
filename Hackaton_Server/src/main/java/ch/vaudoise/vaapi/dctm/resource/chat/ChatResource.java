49505cc9b809422512014a761ccaa305d1443c5e
Hackaton <Hackaton>
2016-11-03 16:55:32 +0100

    remove unused import

--- Hackaton_Server/src/main/java/ch/vaudoise/vaapi/dctm/resource/chat/ChatResource.java
+++ Hackaton_Server/src/main/java/ch/vaudoise/vaapi/dctm/resource/chat/ChatResource.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.Normalizer;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
