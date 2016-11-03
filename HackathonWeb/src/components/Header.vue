4566953427e1a266101c518cd5eb8a3ea8330d29
Jeremy Davis <jeremy.davis@b-i.com>
2016-11-03 14:24:33 +0100

    Create web client

---
+++ HackathonWeb/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <img class="logo" src="../assets/va-logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 120px;
+  background-color: #007d32;
+
+  .logo {
+    margin: 20px 0 20px 20px;
+  }
+}
+</style>
---
+++ HackathonWeb/src/services/api.js
@@ -0,0 +1,31 @@
+const accessToken = '2fc4af45ff184caa8822a303836982f5'
+const baseUrl = '/api/server-hackaton/'
+// const baseUrl = 'https://api.api.ai/v1/'
+// const baseUrl = 'http://192.168.1.85:8080/server-hackaton/'
+
+import _ from 'lodash'
+import { guid } from './utils'
+
+export function sendMessage (message) {
+  return window.fetch(baseUrl + 'chat', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken
+    },
+    body: JSON.stringify({ q: message, lang: 'fr', sessionId: guid })
+  })
+  .then((response) => {
+    try {
+      return response.json()
+    } catch (e) {
+      console.log(e)
+      return _.set({}, 'result.fulfillment.speech', 'Une erreur est survenue, pouvez-vous me réexpliquer?')
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+    return 'Une erreur est survenue'
+  })
+}
